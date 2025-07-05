d0fb1718ef6dc275abe7fb84481d15ac450ae007
jtaddrocket <nguyentiendat3_t66@hus.edu.vn>
2025-07-06 01:39:23 +0700

    update Jinja2 template engine

--- jinja.py
+++ jinja.py
@@ -7,7 +7,7 @@
 {#...#} this is for comments
 '''
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 '''
 It creates an instance of the Flask class,
 which will be your WSGI (Web Server Gateway Interface) application.
@@ -15,12 +15,12 @@ which will be your WSGI (Web Server Gateway Interface) application.
 ### WSGI Application
 app = Flask(__name__)
 
-@app.route("/submit",methods=["GET", "POST"])
-def form():
-    if request.method == "POST":
-        name=request.form['name']
-        return f'Hello {name}!'
-    return render_template("form.html")
+# @app.route("/submit",methods=["GET", "POST"])
+# def form():
+#     if request.method == "POST":
+#         name=request.form['name']
+#         return f'Hello {name}!'
+#     return render_template("form.html")
 
 @app.route("/success/<int:score>")
 def success(score):
@@ -47,6 +47,27 @@ def successres(score):
 def successif(score):
 
     return render_template("result2.html", result=score)
+
+
+@app.route("/fail/<int:score>")
+def fail(score):
+    return render_template("result.html", result=score)
+
+@app.route("/submit", methods=["GET", "POST"])
+def submit():
+    total_score = 0 
+    if request.method == "POST":
+        science = float(request.form['science'])
+        maths = float(request.form['maths'])
+        c = float(request.form['c'])
+        datascience = float(request.form['datascience'])
+
+        total_score = (science + maths + c + datascience) / 4
+    else:
+        return render_template("getresult.html")
+
+    return redirect(url_for('successres', score=total_score)   )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
